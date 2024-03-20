e032a48ea816e24161aeea59b3f2fc4058b3ef74
AaronMauget <amauget@hotmail.com>
2024-03-20 16:34:55 -0700

    revisit removeFromArray:
    sumALL:
    create an array for input values
    .sort() array to order from lowest to highests.
    assign lowest and highest vars from array
    impliment for loop to add from low to high var.
    return ERROR for string types and nested arrays.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+     // create a new empty array
+  const newArray = [];
+  // use forEach to go through the array
+  array.forEach((item) => {
+    
+    if (!args.includes(item)) {
+      newArray.push(item);
+    }
+  });
+  // and return that array
+  return newArray;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,29 @@
-const sumAll = function() {
+const sumAll = function(numberOne, numberTwo){
+    
+    
+    let array = [numberOne,numberTwo];
 
-};
-
+    let sortedArray = array.sort();
+    //Sorts array from lowest to highest to target in for loop.
+    
+    let sum = 0;
+    if (sortedArray[0]< 0){
+        return "ERROR";
+    }
+    else if(typeof(sortedArray[1])==="string"){
+        //Elements sorted will put strings last. Thus the fixed position.
+        return "ERROR"
+    }
+    else if(sortedArray[1].length > 0){
+        return "ERROR";
+        //again, anything not a numbertype is sorted to the last slot. 
+    }
+    else{
+        for (i = sortedArray[0]; i <=sortedArray[1]; i++){
+            sum +=i;
+        }
+        return sum;
+        }
+    }
 // Do not edit below this line
 module.exports = sumAll;
