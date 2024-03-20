2200eb99643387187fa3b92813af6ac2740ed462
AaronMauget <amauget@hotmail.com>
2024-03-20 10:45:20 -0700

    remove .skip for repeat.spec.js tests
    repeatString.js:
    create greet and times var
    
    return greet.repeat([times])
    return negative "times" values as "ERROR"

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
 
+const repeatString = function(greet,times) {
+    if (times < 0){
+        return "ERROR"
+    }
+    else{
+        return greet.repeat([times]);
+    }
 };
 
 // Do not edit below this line
