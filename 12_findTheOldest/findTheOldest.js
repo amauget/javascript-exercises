4945590e2a02e6971825e88edd570b1ba4414a65
AaronMauget <amauget@hotmail.com>
2024-03-21 16:59:01 -0700

    determine age via date of death - birth create array of age use Math.max() to find oldest age create parallel array of associated names create for "i" of loop to return oldest position return name[i]
    BUG:
    name returns undefined.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let age = people.map((person) => (person.yearOfDeath - person.yearOfBirth))
 
+    let name = people.map((person) => (person.name));
+    // both age and name arrays have the same order since they pulled from the same nest
+    let oldest = (Math.max(...age));
+    //... is spread syntax it expands array into individual values.
+    //allowing for max to be identified.
+
+    for (i in age){
+        if (age[i] === oldest){
+            return name[i];
+            
+        }
+    }
 };
 
 // Do not edit below this line
