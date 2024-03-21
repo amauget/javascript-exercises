dcb75e4adf85aca7dccd1cb82aaa2afb0eef8c95
AaronMauget <amauget@hotmail.com>
2024-03-21 16:06:41 -0700

    extract objects from nested array target object keys "title" and assign to vars push() title vars to empty array "titleArray" return titleArray

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,4 +1,15 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    // You'll be given title and author of books. return array of titles
+
+    let book1 = books[0]; book2 = books[1];
+    //extracts nested objects from array
+    let title1 = book1.title; title2 = book2.title;
+    //targets object key "title"
+    let titleArray = []
+
+    titleArray.push(title1, title2);
+
+    return titleArray;
 
 };
 
