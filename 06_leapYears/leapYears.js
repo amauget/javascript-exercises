57c1581750d320c8f83fbd65f8977c52e4dd3f90
AaronMauget <amauget@hotmail.com>
2024-03-21 12:07:04 -0700

    Enable all tests determine Fahr and Cel conversion formulas return int for .0 rounding return float for non .0 rounding
    append OPrjct solution to first formula

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,12 @@
+//README detail: leap year is defined as year % 4 = 0 unless start of century.
+// if start of century, year % 400 = 0 to be leap
+
 const leapYears = function(year) {
-const leap = (((year % 4 === 0) &&(year % 100 !== 0) || (year % 400 === 0)));
-return leap;
+
+    const leap = (((year % 4 === 0) &&(year % 100 !== 0) || (year % 400 === 0)));
+                //First condition accounts for non century. Second accounts for century.
+                
+    return leap;
 }
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,34 @@
-const convertToCelsius = function() {
+
+const convertToCelsius = function(f) {
+  //PROVIDED SOLUTION:
+  // return Math.round((fahrenheit - 32) * (5 / 9) * 10) / 10;
+  // multiplies decimal val by 10, rounds to whole int, then divides by 10 to 
+  //give tenth position if exists.
+
+  let celsius =((f - 32) * (5/9)).toFixed(1)
+        if(celsius[-1] === "0"){
+           return parseInt(celsius);
+        }
+        else{
+             
+            return parseFloat(celsius);
+        }
+        
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+    
+  let fahrenheit = (((9/5 * c) + 32)).toFixed(1);
+  if(fahrenheit[-1] === "0"){
+    return parseInt(fahrenheit);
+   
+  }
+ else{
+     return parseFloat(fahrenheit);
+     
+ }
+
+ 
 };
 
 // Do not edit below this line
