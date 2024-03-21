c21486f4a2138bc99f2447ebebf6bd5bd3f7861b
AaronMauget <amauget@hotmail.com>
2024-03-21 15:30:37 -0700

    plug input variable into function & convert to int assign input to var "max" create temp vars a and b for sequence create seq = [0]
    create conditional for handling negative inputs
    create conditional for returning 0 and 1
    
    create while seq[max-1]===undefined
    update vars a & b
    push() to seq until seq[max-1] is defined.
    return value in seq[max-1]

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,33 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+    // fibonacci sequence is where each number is the sum of the 2 preceding nums
+    // F(n) = F(n-1) + F(n-2)
+    
+    let a = 1, b = 0, temp;
+    //temp used for updating variables
+    let seq = [0];
 
-};
-
+    let max = parseInt(input);
+    //Allows for str entries
+    
+    if (max < 0){
+        return "OOPS";
+    }
+    else if(max <= 1){
+        return max;
+        // eliminates math hole in while loop since max = value at that place up to 1.
+    }
+    else{
+        while(seq[max-1] === undefined){
+        //Continues until array fills the position of max-1
+        temp = a;
+        a = a + b;
+        b = temp;
+        seq.push(a);
+        }
+    return seq[max-1];
+    }
+    
+}
+   
 // Do not edit below this line
 module.exports = fibonacci;
