ac84b3db98a34daf218697600ba770859cf4cd94
AaronMauget <amauget@hotmail.com>
2024-03-21 10:43:47 -0700

    Enable tests. assign leap variable leap accounts for century and non cent numbs return leap: true for %=0 and false for %!=0

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,7 @@
-const leapYears = function() {
-
-};
+const leapYears = function(year) {
+const leap = (((year % 4 === 0) &&(year % 100 !== 0) || (year % 400 === 0)));
+return leap;
+}
 
 // Do not edit below this line
 module.exports = leapYears;
