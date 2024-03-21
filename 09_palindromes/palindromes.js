4a057834075053e9938e18647c113d56a0e92417
AaronMauget <amauget@hotmail.com>
2024-03-21 14:17:31 -0700

    Strip non-alphanum chars from Str create var for reveresed Str return true if reversed === Str return false otherwise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) { 
+    let stripString = (str.replace(/[^\w\']|_/g, "")).toLowerCase();
+    // Removes all non Alphanumerics and whitespaces. To keep whitespaces, add \s after \w
+    let reverseStr = stripString.split('').reverse().join('');
+    //splits all letters, reverses order, rejoins.
+    
+    return reverseStr === stripString ? true : false;
+    //shorthand if...else conditional 
+     
 
 };
 
