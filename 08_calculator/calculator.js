23a46f2eda6bccc7649142d7a74fedbcb5388479
AaronMauget <amauget@hotmail.com>
2024-03-21 13:24:36 -0700

    implement for add,sum,minus,mult,power & factorial

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,55 @@
-const add = function() {
+const add = function(num1, num2) {
+  const add = num1 + num2;
+  return add;
+};
+
+const subtract = function(num1, num2) {
+	const minus = (num1 - num2);
+  //parenthesis matter! 
+  return minus;
+};
+
+const sum = function(array) {
+  if (array.length === 0){
+    return 0;
+  }
+  else{
+    let sum = 0;
+    for (i of array){
+      sum += i;
+    }
+    return sum;
+  }
 	
 };
 
-const subtract = function() {
+const multiply = function(array) {
+  if (array.length === 0){
+    return 0;
+  }
+  else{
+    let product = 1;
+    for (i of array){
+      product *= i;
+    }
+    return product;
+  }
+
+};
+
+const power = function(num1, num2) {
+  let toPower = num1 ** num2;
+  return toPower;
 	
 };
 
-const sum = function() {
-	
-};
+const factorial = function(number) {
+  let multiplier = 1;
+  for (i=1; i <= number; i++){
+    multiplier *= i;
+  }
+  return multiplier;
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
 };
 
 // Do not edit below this line
